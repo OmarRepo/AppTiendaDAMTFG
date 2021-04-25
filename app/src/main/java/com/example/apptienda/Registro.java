5004b3ba344ba9335eb77f21b365f72a57732fdf
ShiroTendo <sergiofernandez.cue@gmail.com>
2021-04-25 20:05:28 +0200

    Prueba_Comentario

--- app/src/main/java/com/example/apptienda/Registro.java
+++ app/src/main/java/com/example/apptienda/Registro.java
@@ -14,6 +14,7 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        //HolaMorta
     }
     public void calendario(View view){
         EditText et=findViewById(R.id.fecha_registro);
