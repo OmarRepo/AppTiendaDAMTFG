5c91905569132f734a0f056ac5777927a26a9b59
ShiroTendo <sergiofernandez.cue@gmail.com>
2021-05-06 22:54:06 +0200

    Ventana_Informacion_Usuario

---
+++ app/src/main/java/com/example/apptienda/perfil_usuario.java
@@ -0,0 +1,14 @@
+package com.example.apptienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfil_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+    }
+}
