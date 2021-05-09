87e933d4784d0c82392d5755e353be46a5f01459
ShiroTendo <sergiofernandez.cue@gmail.com>
2021-05-09 12:48:33 +0200

    Ventana_Sobre_Nosotros

---
+++ app/src/main/java/com/example/apptienda/SobreNosotros.java
@@ -0,0 +1,21 @@
+package com.example.apptienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SobreNosotros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre_nosotros);
+    }
+    public void volverTienda(View view){
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+
+    }
+}
